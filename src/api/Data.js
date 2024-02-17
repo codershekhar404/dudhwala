f6dd5e3aaab09a9f97a58774a1051de9c212d8e4
codershekhar <codershekhar404@gmail.com>
2024-02-17 12:41:33 +0530

    Added Buffalo Milk

--- src/api/Data.js
+++ src/api/Data.js
@@ -13,24 +13,30 @@ const Data = [
   },
   {
     id: 3,
+    name: "Buffalo Milk",
+    price: 67,
+    unit: "Kg",
+  },
+  {
+    id: 4,
     name: "Super Tea",
     price: 8.5,
     unit: "Pcs",
   },
   {
-    id: 4,
+    id: 5,
     name: "Dahi",
     price: 17,
     unit: "Pcs",
   },
   {
-    id: 5,
+    id: 6,
     name: "Tadka",
     price: 9.1,
     unit: "Pcs",
   },
   {
-    id: 6,
+    id: 7,
     name: "Eco",
     price: 9.5,
     unit: "Pcs",
